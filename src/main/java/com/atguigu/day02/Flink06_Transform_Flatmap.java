187f998d6e106c4833c5aff45409d3df597cd200
TangZC <TangZC@atguigu.com>
2020-11-17 16:38:07 +0800

    flink0621

---
+++ src/main/java/com/atguigu/day02/Flink06_Transform_Flatmap.java
@@ -0,0 +1,30 @@
+package com.atguigu.day02;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author TangZC
+ * @create 2020-11-17 16:14
+ */
+public class Flink06_Transform_Flatmap {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<String> fileDS = env.readTextFile("sensor");
+
+        SingleOutputStreamOperator<String> stringSingleOutputStreamOperator = fileDS.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String s, Collector<String> collector) throws Exception {
+                String[] split = s.split(",");
+                for (String s1 : split) {
+                    collector.collect(s1);
+                }
+            }
+        });
+    }
+}
